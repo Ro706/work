c49463a61ff63889af131ebcbe9ea2c22f0820b7
Rohit Mandal <60247178+Ro706@users.noreply.github.com>
2022-12-04 11:33:41 +0530

    update

--- Day_1/vowel_counts.py
+++ Day_1/vowel_counts.py
@@ -1,15 +1,11 @@
 a_String=str(input(""))
 lowercase = a_String.lower()
-
 vowel_counts = {}
-
 for vowel in "aeiou":
     count = lowercase.count(vowel)
-
     vowel_counts[vowel] = count
 counts = vowel_counts.values()
-
 total_vowels = sum(counts)
-
 print(total_vowels)
+print("made by ro404")
   
